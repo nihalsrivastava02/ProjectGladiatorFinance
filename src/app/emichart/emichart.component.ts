5a2f3e0b2468299f0c69cbcfd51143f2cee0f768
Akshat Singhal <42622140+Iamakshat98@users.noreply.github.com>
2020-08-26 11:16:46 +0530

    Update emichart.component.ts

--- src/app/emichart/emichart.component.ts
+++ src/app/emichart/emichart.component.ts
@@ -1,23 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  dueid: number;
+  purchasetxnid:number;
+  duedate:String;
+  paymenttobedone:number;
+  initialtobebalance:number;
+  afterpaymenttobebalance:number;
+  installmentmonthno:number;
+  status:String;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:2,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:2,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:1,	status:'complete'},
+  {dueid:10410311,	purchasetxnid:141031,	duedate:"sdfs",	paymenttobedone:8333.33,	initialtobebalance:50000,	afterpaymenttobebalance:41666.77,	installmentmonthno:2,	status:'complete'},
+
+  
 ];
 
 @Component({
@@ -26,10 +63,12 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./emichart.component.css']
 })
 
-
-
-
 export class EmichartComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['dueid','purchasetxnid',	'duedate',	'paymenttobedone',	'initialtobebalance',	'afterpaymenttobebalance',	'installmentmonthno',	'status'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
